8320842b7e98610adbe828266a36a0f4caaccd09
dreamfish <yxlumengyu@qq.com>
2024-09-08 22:06:07 +0800

    Site updated: 2024-09-08 22:06:07

--- live2d-widget/src/index.js
+++ live2d-widget/src/index.js
@@ -132,7 +132,7 @@ function loadWidget(config) {
     if (modelId === null) {
       // 首次访问加载 指定模型 的 指定材质
       modelId = 4; // 模型 ID
-      modelTexturesId = 115; // 材质 ID
+      modelTexturesId = 141; // 材质 ID
     }
     model.loadModel(modelId, modelTexturesId);
     fetch(config.waifuPath)
